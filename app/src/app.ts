4aee1ff124dc88f713e269a335ec55658f630089
Theo Gregory <tjmg@outlook.com>
2020-04-07 21:34:42 +0100

    Linting

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,13 +1,22 @@
-import express from "express";
+import express from "express"
 
-const app = express();
-const port = 3000;
+
+if (!process.env.PORT_APP_LISTEN) {
+    console.error('No environment variable PORT_APP_LISTEN found.')
+    process.exit(1)
+}
+const port = process.env.PORT_APP_LISTEN
+
+const app = express()
+
 app.get("/", (req, res) => {
-  res.send("The sedulous hyena ate the antelope!");
-});
+    res.send("The sedulous hyena ate the antelope!")
+})
+
 app.listen(port, (err) => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`server is listening on ${port}`);
-});
+    if (err) {
+        return console.error(err)
+    }
+
+    return console.log(`server is listening on ${port}`)
+})
