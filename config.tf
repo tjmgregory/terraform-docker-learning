dddbf5e8373d40a3836c4b5f86c24d07c1416038
Theo Gregory <tjmg@outlook.com>
2020-04-07 19:06:36 +0100

    update gitignore

--- config.tf
+++ config.tf
@@ -7,8 +7,8 @@ resource "docker_network" "app-network" {
 }
 
 resource "docker_container" "web" {
-  name = "web"
-  image = "tjmgregory/docker-node-test:1.0"
+  name    = "web"
+  image   = "tjmgregory/docker-node-test:1.0"
   restart = "always"
   ports {
     internal = "80"
@@ -20,19 +20,19 @@ resource "docker_container" "web" {
 }
 
 resource "docker_container" "postgres" {
-  name = "postgres"
-  image = "postgres:12.2"
+  name    = "postgres"
+  image   = "postgres:12.2"
   restart = "always"
-  env = ["POSTGRES_PASSWORD=password"]
+  env     = ["POSTGRES_PASSWORD=password"]
   networks_advanced {
     name = "app-network"
   }
   volumes {
-    host_path = "/Users/theo/learning/postgres_data"
+    host_path      = "/Users/theo/learning/postgres_data"
     container_path = "/var/lib/postgresql/data"
   }
   ports {
     internal = "5432"
     external = "6969"
   }
-} 
+}
