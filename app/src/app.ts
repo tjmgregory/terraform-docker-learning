7bd733404b5795fb6fa1592c5efc12fbcc75784a
Theo Gregory <tjmg@outlook.com>
2020-04-07 22:38:42 +0100

    First cut at reading database

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,16 +1,28 @@
 import express from "express"
+import { pgPool } from "./sql/config"
 
-
-if (!process.env.PORT_APP_LISTEN) {
-    console.error('No environment variable PORT_APP_LISTEN found.')
+if (!process.env.APP_PORT) {
+    console.error('No environment variable APP_PORT found.', process.env)
     process.exit(1)
 }
-const port = process.env.PORT_APP_LISTEN
+const port = process.env.APP_PORT
 
 const app = express()
 
-app.get("/", (req, res) => {
-    res.send("The sedulous hyena ate the antelope!")
+app.get("/", async (_req, res) => {
+    try {
+        const users = await pgPool().query('SELECT * FROM users')
+        if (users.rowCount === 0) {
+            console.log('Failed to find any users.')
+            res.status(404).send('Failed to find any users.')
+            return
+        }
+
+        res.send("The sedulous hyena ate the antelope!")
+    } catch (err) {
+        console.error('Error when fetching users.', err)
+        res.status(500).send(`Error when fetching users. ${err.message}`)
+    }
 })
 
 app.listen(port, (err) => {
---
+++ app/src/sql/config.ts
@@ -0,0 +1,24 @@
+import { Pool, PoolConfig } from "pg"
+
+let _pgPool: Pool
+export function pgPool(): Pool {
+    return _pgPool || (_pgPool = setupPgPool())
+}
+
+function setupPgPool(): Pool {
+    if (!process.env.DB_HOST || !process.env.DB_PORT || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
+        console.error('Missing db environment variables.', process.env)
+        process.exit(1)
+    }
+
+    const port = parseInt(process.env.DB_PORT)
+
+    const config: PoolConfig = {
+        max: 1,
+        host: process.env.DB_HOST,
+        port,
+        user: process.env.DB_USER
+    }
+
+    return new Pool(config)
+}
