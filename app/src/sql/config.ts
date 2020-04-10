23e106bbe482fec294a80fbc1211582d442ce161
Theo Gregory <tjmg@outlook.com>
2020-04-10 21:01:12 +0100

    Adds test env vars

--- app/src/sql/config.ts
+++ app/src/sql/config.ts
@@ -17,7 +17,9 @@ function setupPgPool(): Pool {
         max: 1,
         host: process.env.DB_HOST,
         port,
-        user: process.env.DB_USER
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     }
 
     return new Pool(config)
